b639403f08da31d00f18fcfa85c4e2d6584fdd5a
Sanket Kadam <97290722+Sanketkadam3921@users.noreply.github.com>
2025-08-15 17:20:05 +0530

    Update auth.routes.js

--- server/dist/routes/auth.routes.js
+++ server/dist/routes/auth.routes.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
+const express_1 = require("express"); 
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const user_model_1 = __importDefault(require("../models/user.model"));
