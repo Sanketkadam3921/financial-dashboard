ce98adf95ce996271bf13bb737b9bd961929ae69
Sanket Kadam <97290722+Sanketkadam3921@users.noreply.github.com>
2025-08-15 17:12:54 +0530

    Update ExportModal.tsx

--- frontend/src/features/export/ExportModal.tsx
+++ frontend/src/features/export/ExportModal.tsx
@@ -7,6 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const { Text, Title } = Typography;
 
+
 const fieldOptions = [
     { label: 'Date', value: 'date' },
     { label: 'Amount', value: 'amount' },
